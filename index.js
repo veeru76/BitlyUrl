a889c5b1a338d278d1b51aa55efd1886bbfdb79d
veeru76 <veeranjaneyaveeru@gmail.com>
2021-09-28 19:33:12 +0530

    shortening with bitly apis

--- index.js
+++ index.js
@@ -1,14 +1,48 @@
-
+const axios = require('axios').default;;
 const express = require("express");
+const bodyParser = require('body-parser');
 const app = express();
 
-app.get('/',(req, res) => {
+
+// support parsing of application/json type post data
+app.use(bodyParser.json());
+
+// support parsing of application/x-www-form-urlencoded post data
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+
+app.get('/', (req, res) => {
     res.send("hello world")
 })
 
-app.post('/v4/shorten', (req, res) => {
-    const url = req.body.url;
+app.post('/shorten', async (req, res) => {
+    const url = req.body.long_url;
+    const token = '4c6e039b4d2997fd42d972d39715b945180f2369';
+
+
+    var getGroupId = (() => {
+        var _groupId = null;
+        return async () => {
+            if (_groupId == null) {
+                var response = await axios.get('https://api-ssl.bitly.com/v4/groups', {
+                    headers: {
+                        'Authorization': `Bearer ${token}`
+                    }
+                })
+                console.log(response);
+            }
+            return _groupId;
+        }
+    });
+    await getGroupId();
+    // var response = await axios.get('https://api-ssl.bitly.com/v4/groups?organization_guid=Oa1bcd234eF')
+    // console.log(response.groups.guid)
     
+    // let shortUrl = await axios.get("https://api-ssl.bitly.com/v4/shorten")
+    // return shortUrl;
+    res.status(200).send('a');
 })
 
-app.listen('9000', () => console.log('server started'))
+app.listen('8081', () => console.log('server started'))
